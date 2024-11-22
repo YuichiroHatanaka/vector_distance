e156e4dc9ca918de539b3fb867bf97ff2cea669a
YuichiroHatanaka <yuichirohata1121@gmail.com>
2024-11-22 20:38:58 +0900

    add map data

--- irreversible/irreversible.c
+++ irreversible/irreversible.c
@@ -37,6 +37,9 @@ int main(void){
 	printf("代表ベクトルは%d個\n", representative_vector_num);
 	fseek(codebook_fp, 0, SEEK_SET);
 	
+	//g個のベクトルを比較
+	for(int g = 0; g < 2; g++){
+	fseek(codebook_fp, 0, SEEK_SET);
 	//ベクトルを２つ読み込み
 	fread(data1, sizeof(char), element_num, block_fp);
 	//マップから読み込んだベクトルを端末に表示
@@ -45,15 +48,13 @@ int main(void){
 		printf("%02x ", data1[i]);
 	//h個の代表ベクトル
 	for(int h = 0; h < representative_vector_num; h++){
-	same_element_num = 0;
-	diff_element_num = 0;
-
-	fread(data2, sizeof(char), element_num, codebook_fp);
+		same_element_num = 0;
+		diff_element_num = 0;
+		fread(data2, sizeof(char), element_num, codebook_fp);
 	//代表ベクトルを端末に表示
-		printf("\ndata2: ");
+		printf("\n%d個目の代表ベクトル: ", h+1);
 		for(int i = 0; i < element_num; i++)
 			printf("%02x ", data2[i]);
-		printf("\n");	
 	//h番目の代表ベクトルと比較
 		for(int i = 0; i < element_num; i++){
 			if(data1[i] == data2[i]){
@@ -62,13 +63,14 @@ int main(void){
 				diff_element_num++;
 			}
 		}
-		if(same_element_num == element_num){
-			printf("符号帳の%d番目の代表ベクトルと一致\n", h + 1);
+		if(diff_element_num <= 0){
+			printf("\n符号帳の%d番目の代表ベクトルと一致\n\n", h + 1);
 			break;
 		}
-	//どれくらい違ったかを記述
-		printf("同じ要素は%d個\n", same_element_num);
-		printf("違う要素は%d個\n\n", diff_element_num);
+	//どれくらい違ったかを記述（距離を測るのに使う）
+		//printf("同じ要素は%d個\n", same_element_num);
+		//printf("違う要素は%d個\n\n", diff_element_num);
+	}
 	}
 	
 	fclose(block_fp);
