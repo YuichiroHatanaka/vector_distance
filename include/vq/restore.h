e78df5f6acd9bd37419bd70dfd517e2d65b968e3
YuichiroHatanaka <yuichirohata1121@gmail.com>
2024-11-23 20:42:42 +0900

    ir test

---
+++ include/vq/restore.h
@@ -0,0 +1,8 @@
+#ifndef __RESTORE_H__
+#define __RESTORE_H__
+
+#include<stdio.h>
+
+int restore(char codebook[], char vq[], char restore[], int width, int height, int start, int rowElem, int colElem, char reData[], int row, int relength);
+
+#endif
