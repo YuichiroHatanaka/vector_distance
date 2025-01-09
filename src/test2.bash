447b790195301f7b534b162bc8b9ac31184f20f7
YuichiroHatanaka <yuichirohata1121@gmail.com>
2025-01-09 20:50:15 +0900

    test2

---
+++ src/test2.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2024 Yuichiro Hatanaka
+# SPDX-License-Identifier: BSD-3-Clause
+
+distance=$1
+if [ -z "" ]; then
+distance=0
+fi
+distance=$2
+if [ -z "" ]; then
+distance=0
+fi
+gcc irreversible.c restore.c -o out -I ../include
+./out "$distance" > data.log
+rm out
